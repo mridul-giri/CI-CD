1be35fb55ee6990834817898f86b9224b05112e1
Mridul Giri <mridulgiri2@gmail.com>
2025-07-18 16:26:31 +0530

    init

---
+++ apps/http-server/src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import { client } from "@repo/db/client";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("hi theres"));
+app.post("/signup", (req, res) => {
+  const { username, password } = req.body;
+  const user = client.user.create({
+    data: { username, password },
+  });
+  res.json({ message: "signup successfully", user });
+});
+
+app.listen(3000, () => console.log("app is listening on port 3000"));
