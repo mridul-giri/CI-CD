3cb6bb233694cef1a5d083fd5d3763c544ad788c
Mridul Giri <mridulgiri2@gmail.com>
2025-07-20 23:25:00 +0530

    add nginx conf

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -13,4 +13,4 @@ app.post("/signup", (req, res) => {
   res.json({ message: "signup successfully", user });
 });
 
-app.listen(3000, () => console.log("app is listening on port 3000"));
+app.listen(3002, () => console.log("app is listening on port 3002"));
--- apps/ws-server/src/index.ts
+++ apps/ws-server/src/index.ts
@@ -5,8 +5,8 @@ const server = new WebSocketServer({
   port: 3001,
 });
 
-server.on("connection", (socket) => {
-  client.user.create({
+server.on("connection", async (socket) => {
+  const res = await client.user.create({
     data: {
       username: Math.random().toString(),
       password: Math.random().toString(),
