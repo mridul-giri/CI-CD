981037bf626bd7d8a37012cde1e54c9590fd6eda
Mridul Giri <mridulgiri2@gmail.com>
2025-07-24 00:23:36 +0530

    add username & password on web app

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,7 +4,9 @@ export default async function Home() {
   const user = await client.user.findFirst();
   return (
     <div>
+      Username:
       {user?.username}
+      Password:
       {user?.password}
     </div>
   );
