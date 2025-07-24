b80660a3a739386934a3e3fff6479510f3049f89
Mridul Giri <mridulgiri2@gmail.com>
2025-07-24 14:10:51 +0530

    update web app

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,7 +4,7 @@ export default async function Home() {
   const user = await client.user.findFirst();
   return (
     <div>
-      Username:
+      Name:
       {user?.username}
       Password:
       {user?.password}
